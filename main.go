f9529b8960c04e810c142a3c71c727f7982b76ed
scottw0173 <swarner5937@gmail.com>
2026-04-04 19:00:58 -0700

    added basic command line REPL using bufio and os

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()
+		text := scanner.Text()
+
+		words := cleanInput(text)
+		if len(words) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
 }
