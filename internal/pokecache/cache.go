fe64ce9be6eb9334b5bde4de2dd49f7876b8ca15
scottw0173 <swarner5937@gmail.com>
2026-04-07 19:31:23 -0600

    started cache, saving progress

---
+++ internal/pokecache/cache.go
@@ -0,0 +1,16 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	cacheEntries map[string]cacheEntry
+	mu           sync.Mutex
+}
